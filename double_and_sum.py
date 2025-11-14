8440bcc9038942c6b895133944e7da0c6c7f8808
Liam Kelly <liam.james.kelly@gmail.com>
2025-11-14 06:17:46 +0000

    Refactor double_and_sum function for improved structure and readability

--- double_and_sum.py
+++ double_and_sum.py
@@ -1,10 +1,16 @@
-# double_and_sump.py
+# double_and_sum.py
 
-data = [1, 2, 3, 4, 5]
-print("data =", data)
 
-doubled = [2 * x for x in data]
-print("doubled =", doubled)
+def double_and_sum():
+    data = [1, 2, 3, 4, 5]
+    print("data =", data)
 
-total = sum(doubled)
-print("total =", total)
+    doubled = [2 * x for x in data]
+    print("doubled =", doubled)
+
+    total = sum(doubled)
+    print("total =", total)
+
+
+if __name__ == "__main__":
+    double_and_sum()
