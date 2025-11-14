bc56c0e75a5bfde7b9a2e2a699a7b2356a8d6c34
Liam Kelly <liam.james.kelly@gmail.com>
2025-11-14 05:29:16 +0000

    Initial script

---
+++ double_and_sum.py
@@ -0,0 +1,10 @@
+# double_and_sump.py
+
+data = [1, 2, 3, 4, 5]
+print("data =", data)
+
+doubled = [2 * x for x in data]
+print("doubled =", doubled)
+
+total = sum(doubled)
+print("total =", total)
