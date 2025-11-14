ab7ed87ed7821f8b1562cb954feb089b9cb81873
Liam Kelly <liam.james.kelly@gmail.com>
2025-11-14 06:45:00 +0000

    Add TaskFn type alias for improved type clarity

--- src/pluck/protocols/typing_.py
+++ src/pluck/protocols/typing_.py
@@ -1,9 +1,10 @@
 # src/pluck_example/protocols/typing_.py
 
-
+from collections.abc import Callable
 from typing import Any, TypeAlias
 
 
 StoreData: TypeAlias = Any
 TaskData: TypeAlias = Any
+TaskFn: TypeAlias = Callable[..., StoreData]
 TaskId: TypeAlias = Any
